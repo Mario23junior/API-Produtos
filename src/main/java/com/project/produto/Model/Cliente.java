bc585bb7f4276954119f4f832452738e6e8ff121
Mario23junior <mariojunior3251@gmail.com>
2021-01-27 03:56:45 -0300

    Criando modelo de Clientes

---
+++ src/main/java/com/project/produto/Model/Cliente.java
@@ -0,0 +1,55 @@
+package com.project.produto.Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente {
+      
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	private String nome;
+	
+	@Column(length = 11)
+	private String cpf;
+	
+	@Column(length = 20)
+	private String telefone;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+}
