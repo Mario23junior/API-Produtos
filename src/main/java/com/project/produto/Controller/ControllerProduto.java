f042682d79153e369998bd50f2a792a956d16a22
Mario23junior <mariojunior3251@gmail.com>
2021-01-28 06:03:17 -0300

    listando produto por id

--- src/main/java/com/project/produto/Controller/ControllerProduto.java
+++ src/main/java/com/project/produto/Controller/ControllerProduto.java
@@ -1,11 +1,14 @@
 package com.project.produto.Controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.project.produto.Model.Produto;
 import com.project.produto.Resitory.ProdutoRepository;
@@ -26,7 +29,12 @@ public class ControllerProduto {
 		return produtoRepository.save(produto);
 	}
 	
-
+	@GetMapping("/{id}")
+	public Produto obterProdutoPorId(@PathVariable Integer id) {
+		return produtoRepository
+				     .findById(id)
+				     .orElseThrow(() -> new ResponseStatusException(HttpStatus.NO_CONTENT,"Produto não encontrado"));
+	}
 }
 
 
