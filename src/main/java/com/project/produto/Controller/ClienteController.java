1d11a186a2d72fdc65707bf76e2328b86fdb654a
Mario23junior <mariojunior3251@gmail.com>
2021-01-28 03:12:37 -0300

    Atualizando clientes na base por id

--- src/main/java/com/project/produto/Controller/ClienteController.java
+++ src/main/java/com/project/produto/Controller/ClienteController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -47,6 +48,20 @@ public class ClienteController {
 		                	 return deleteCliente;
 		                 }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Cliente não encontrado para exclusao"));
 	}
+	
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void updateCli(@PathVariable Integer id ,@RequestBody Cliente cliente) {
+		clienteRepository.findById(id)
+		                 .map(clienteBody -> {
+		                	 cliente.setId(clienteBody.getId());
+		                	 clienteRepository.save(cliente);
+		                	 return clienteBody;
+		                 }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Cliente não encontrado na base"));
+	}
+ 	
+	
+	
 	 
 	
 	
