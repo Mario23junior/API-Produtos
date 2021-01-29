288fd372a4b5f0afd0147d82e5c65870e9a290ea
Mario23junior <mariojunior3251@gmail.com>
2021-01-29 03:33:07 -0300

    Criando modelo dos atributos a serem exibidos no cliente e produto

--- src/main/java/com/project/produto/DTO/ClienteProdutoDTO.java
+++ src/main/java/com/project/produto/DTO/ClienteProdutoDTO.java
@@ -1,7 +1,77 @@
 package com.project.produto.DTO;
 
+import java.math.BigDecimal;
+
 public class ClienteProdutoDTO {
     
-
+	private String ClientId;
+	private String nome;
+	private String email;
+	private String telefone;
+	
+	private String nomeProd;	
+	private String descricao;
+	private BigDecimal preco;
+	private boolean vendido;
+	private Integer quantidade;
+	
+	
+	public String getClientId() {
+		return ClientId;
+	}
+	public void setClientId(String clientId) {
+		ClientId = clientId;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getNomeProd() {
+		return nomeProd;
+	}
+	public void setNomeProd(String nomeProd) {
+		this.nomeProd = nomeProd;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public BigDecimal getPreco() {
+		return preco;
+	}
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+	public boolean isVendido() {
+		return vendido;
+	}
+	public void setVendido(boolean vendido) {
+		this.vendido = vendido;
+	}
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	
+	
 	
 }
