9c4a990319b376a9f3039a3734182359031000f8
Mario23junior <mariojunior3251@gmail.com>
2021-01-31 04:13:21 -0300

    Criando classe a ser implementada

---
+++ src/main/java/com/project/produto/Service/ServiceDTO.java
@@ -0,0 +1,5 @@
+package com.project.produto.Service;
+
+public interface ServiceDTO {
+
+}
---
+++ src/main/java/com/project/produto/Service/implementsDTO/ClientProdutoImple.java
@@ -0,0 +1,7 @@
+package com.project.produto.Service.implementsDTO;
+
+import com.project.produto.Service.ServiceDTO;
+
+public class ClientProdutoImple implements ServiceDTO {
+
+}
